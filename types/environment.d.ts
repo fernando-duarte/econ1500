9ad15b97b0fb257d313b89aa0dae80ab7c1335a8
Fernando Duarte <duarte@alum.mit.edu>
2025-05-04 05:57:25 -0400

    Fix environment variables setup and update gitignore

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -4,14 +4,9 @@
  */
 declare namespace NodeJS {
   interface ProcessEnv {
-    // Authentication
-    readonly AUTH_LOGIN_URL: string;
-    readonly AUTH_DEFAULT_REDIRECT: string;
-
     // Public variables (accessible in browser)
     readonly NEXT_PUBLIC_LOGIN_REDIRECT: string;
     readonly NEXT_PUBLIC_LOGOUT_REDIRECT: string;
-    readonly NEXT_PUBLIC_BASE_URL: string;
 
     // Node.js environment
     readonly NODE_ENV: "development" | "production" | "test";
