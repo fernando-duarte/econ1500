70d44fd3fdb1374f34fd2792180af96e1a5d086c
Fernando Duarte <duarte@alum.mit.edu>
2025-05-04 05:33:38 -0400

    Test commit to verify Git hooks with new configuration

--- test-config.js
+++ test-config.js
@@ -1,4 +1,4 @@
 // Test file to verify the Git hooks work with the new configuration files
 const test = "Hello World";
 
-console.log(test)  // Missing semicolon should be caught by linter 
+console.log(test) // Missing semicolon again to test the formatter
