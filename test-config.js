1a0daa30168611ce2fd250fa1e8dd646176f7442
Fernando Duarte <duarte@alum.mit.edu>
2025-05-04 05:34:42 -0400

    Update lint-staged config to specify ESLint config path

--- test-config.js
+++ test-config.js
@@ -1,4 +1,4 @@
 // Test file to verify the Git hooks work with the new configuration files
 const test = "Hello World";
 
-console.log(test) // Missing semicolon again to test the formatter
+console.log(test); // Missing semicolon one more time to test the formatter
