c90efe24020720d1d31b8633a609fe84fa7d1bc4
Fernando Duarte <duarte@alum.mit.edu>
2025-05-07 08:20:25 -0400

    Increase font size of dashboard labels

--- components/game/Dashboard.tsx
+++ components/game/Dashboard.tsx
@@ -10,27 +10,27 @@ export function Dashboard({ prev }: { prev: State }) {
         {/* Capital Box */}
         <Card className="relative flex aspect-square flex-1 flex-col items-center justify-center">
           <div className="text-2xl font-semibold">{prev.K.toFixed(2)}</div>
-          <div className="text-muted-foreground mt-auto flex flex-col items-center pb-2 text-xs">
+          <div className="text-muted-foreground mt-auto flex flex-col items-center pb-2 text-sm">
             <span>Capital</span>
-            <span className="text-xs">(billions USD)</span>
+            <span className="text-sm">(billions USD)</span>
           </div>
         </Card>
 
         {/* Productivity Box */}
         <Card className="relative flex aspect-square flex-1 flex-col items-center justify-center">
           <div className="text-2xl font-semibold">{prev.A.toFixed(3)}</div>
-          <div className="text-muted-foreground mt-auto flex flex-col items-center pb-2 text-xs">
+          <div className="text-muted-foreground mt-auto flex flex-col items-center pb-2 text-sm">
             <span>Productivity</span>
-            <span className="text-xs">(Index, 2017 = 1)</span>
+            <span className="text-sm">(Index, 2017 = 1)</span>
           </div>
         </Card>
 
         {/* Population Box */}
         <Card className="relative flex aspect-square flex-1 flex-col items-center justify-center">
           <div className="text-2xl font-semibold">{prev.L.toFixed(2)}</div>
-          <div className="text-muted-foreground mt-auto flex flex-col items-center pb-2 text-xs">
+          <div className="text-muted-foreground mt-auto flex flex-col items-center pb-2 text-sm">
             <span>Population</span>
-            <span className="text-xs">(millions)</span>
+            <span className="text-sm">(millions)</span>
           </div>
         </Card>
       </div>
@@ -39,9 +39,9 @@ export function Dashboard({ prev }: { prev: State }) {
       {prev.e && (
         <Card className="relative flex flex-col items-center justify-center p-4">
           <div className="text-2xl font-semibold">{prev.e.toFixed(2)}</div>
-          <div className="text-muted-foreground flex flex-col items-center text-xs">
+          <div className="text-muted-foreground flex flex-col items-center text-sm">
             <span>Exchange Rate</span>
-            <span className="text-xs">(CNY per USD)</span>
+            <span className="text-sm">(CNY per USD)</span>
           </div>
         </Card>
       )}
